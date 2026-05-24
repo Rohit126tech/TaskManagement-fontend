ea1e7a4d8ef6418c19f4822b15bf08625ff4c6b4
Rohit Deokar <rdeokar096@gmail.com>
2026-05-24 18:05:43 +0530

    Updated Angular project

--- src/app/features/access-denied-page/access-denied-page.component.ts
+++ src/app/features/access-denied-page/access-denied-page.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-access-denied-page',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './access-denied-page.component.html',
   styleUrl: './access-denied-page.component.css'
 })
