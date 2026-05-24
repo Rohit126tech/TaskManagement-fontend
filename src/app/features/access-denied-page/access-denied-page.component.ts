59680ee52d12d1f5276db0724452eecf6b0c8917
Rohit Deokar <rdeokar096@gmail.com>
2026-05-24 18:13:59 +0530

    Fix standalone component config

--- src/app/features/access-denied-page/access-denied-page.component.ts
+++ src/app/features/access-denied-page/access-denied-page.component.ts
@@ -3,6 +3,7 @@ import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-access-denied-page',
+   standalone: true,
   imports: [RouterLink],
   templateUrl: './access-denied-page.component.html',
   styleUrl: './access-denied-page.component.css'
