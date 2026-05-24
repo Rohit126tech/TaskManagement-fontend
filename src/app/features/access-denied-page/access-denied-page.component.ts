9262b9ce8b96e60c88ac85fd781cbce003bd0f41
Rohit126tech <rdeokar096@gmail.com>
2026-05-24 18:03:28 +0530

    Add RouterLink import to access denied page component

--- src/app/features/access-denied-page/access-denied-page.component.ts
+++ src/app/features/access-denied-page/access-denied-page.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-access-denied-page',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './access-denied-page.component.html',
   styleUrl: './access-denied-page.component.css'
 })
