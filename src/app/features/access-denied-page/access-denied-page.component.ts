6e722ee02f095db20205d8f434ae305eb6e7e3e5
Rohit126tech <rdeokar096@gmail.com>
2026-05-24 18:16:17 +0530

    Add standalone property to access denied component

--- src/app/features/access-denied-page/access-denied-page.component.ts
+++ src/app/features/access-denied-page/access-denied-page.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-access-denied-page',
+   standalone: true,
   imports: [RouterLink],
   templateUrl: './access-denied-page.component.html',
   styleUrl: './access-denied-page.component.css'
